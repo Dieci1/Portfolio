4a23bc754d54aa12ce7f6062bcba358ec556e7e6
Dieci1 <pasqualeioio@libero.it>
2024-02-27 20:09:28 +0100

    feat: Added Contact section and footer

--- script.js
+++ script.js
@@ -2,3 +2,43 @@ window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+//Portfolio section - Modal
+
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) =>{
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () =>{
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+
+//scroll to top button
+
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+})
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
