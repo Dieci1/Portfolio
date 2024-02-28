106638fba5f850189ca6635452628887cbc17a1d
Dieci1 <pasqualeioio@libero.it>
2024-02-28 11:05:16 +0100

    feat: Scroll management to highlight active navigation

--- script.js
+++ script.js
@@ -42,3 +42,19 @@ scrollTopBtn.addEventListener("click", () => {
 })
 
 
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        } else {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+});
