a25632b2e21c0056aa6e07f76326e1ba5e1552ff
Dieci1 <pasqualeioio@libero.it>
2024-03-02 20:27:02 +0100

    fead: Added reveal animation and dark theme button

--- script.js
+++ script.js
@@ -28,6 +28,31 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 });
 
 
+//Website dark/light theme
+
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"] ("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"] ("sun");
+}
+
+
+
 //scroll to top button
 
 const scrollTopBtn = document.querySelector(".scrollToTop-btn");
@@ -58,3 +83,68 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+
+//Download curriculom
+function downloadCurriculum(event) {
+    event.preventDefault(); // Evita il comportamento predefinito del browser
+
+    var curriculumPath = "./materiale/PasqualeIoio.pdf";
+    var downloadLink = document.createElement('a');
+    downloadLink.href = curriculumPath;
+    downloadLink.download = 'PasqualeIoio.pdf';  
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
+var buttons = document.querySelectorAll('.btn'); // Seleziona tutti gli elementi con la classe "btn"
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', downloadCurriculum); // Aggiungi l'evento di click a ciascun bottone
+});
+
+
+//Responsive navigation menu toggle
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
+
+//scroll reveal animations
+
+//common reveal options to create reveal animation
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+//target elements, and specify options to create reveal animation
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', {delay: 500, origin: 'left' });
+ScrollReveal().reveal('.home .info h3, .about-info .btn', {delay: 600, origin: 'right' });
+ScrollReveal().reveal('.home .info .btn', {delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.media-icons i, .contact-left li', {delay: 500, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.about .description, .contact-right', {delay: 600, origin: 'right' });
+ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.skills-description, .service-description, .contact-card, .contact-left h2 ', {delay: 700, origin: 'left' });
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio, .img-card', {delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('footer .group', {delay: 500, origin: 'top', interval: 200 });
+
